0aac88177604038f782a107b9eee5a5818f02993
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2024-12-08 10:42:59 -0500

    feat: test scroll preservation

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -7,4 +7,7 @@
     <!-- Access any shared props -->
     {{ $page.props.auth.user }}
 
+    <!-- Add the preserve-scroll attribute to preserve the scroll throughout webpages -->
+    <!-- <Link class="mt-[1400px] block" href="/" preserve-scroll>Refresh</Link> -->
+
 </template>
